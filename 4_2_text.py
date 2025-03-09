c7e406ee4214aa7211a1ffa887caaa46304f6b9c
drawje <drawje@naver.com>
2025-03-09 16:57:32 +0900

    file and number commit

---
+++ 4_2_text.py
@@ -0,0 +1,12 @@
+print('Hello world')
+print("Hello world")
+print('''
+Hello
+world
+''')
+
+
+print("'1'+'1'",'1'+'1') #문자열과 문자열을 더하면 문자열을 결합하는 결합연산다
+print('Hello world'*1000) #곱하기 연산자를 사용하여 1000번 반복 가능
+print("len('Hello world'*1000)",len('Hello world'*1000)) #len으로 문자열의 길이 알기 가능능
+print("'Hello world'.replace('world','universe')",'Hello world'.replace('world','universe')) #replace를 사용해서 문자열 변환가능
