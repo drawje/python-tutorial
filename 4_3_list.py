312ef9f4acd6b0afa72bcb9fe39a6c5150f65f6f
drawje <drawje@naver.com>
2025-03-09 17:05:26 +0900

    리스트 관련 코드 수정

--- 4_3_list.py
+++ 4_3_list.py
@@ -10,4 +10,4 @@ import statistics #통계와 관련된 모듈
 print("statistics.mean(grades)",statistics.mean(grades))
 
 import random
-print("random.choice(students)",random.choice(students))  #랜덤으로 선택함
+print("random.choice(students)",random.choice(students))  #랜덤으로 선택을 함
